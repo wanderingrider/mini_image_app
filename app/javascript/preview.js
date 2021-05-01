cc6c53a4ecd453be5c73b826cf302e8b8a7607be
wanderingrider <rotarywr250@gmail.com>
2021-05-01 14:24:46 +0900

    画像プレビュー

--- app/javascript/preview.js
+++ app/javascript/preview.js
@@ -1,1 +1,15 @@
-document.addEventListener('DOMContetLoadd')
+document.addEventListener('DOMContentLoaded', function(){
+  const ImageList = document.getElementById('image-list');
+  document.getElementById('message_image').addEventListener('change', function(e){
+    const file = e.target.files[0];
+    const blob = window.URL.createObjectURL(file);
+
+    const imageElement = document.createElement('div');
+
+    const blobImage = document.createElement('img');
+    blobImage.setAttribute('src', blob);
+
+    imageElement.appendChild(blobImage);
+    ImageList.appendChild(imageElement);
+  });
+});
